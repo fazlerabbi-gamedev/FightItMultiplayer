a7ffd9a7d8aef9c9d1e5495953899b0fdd29bf58
Fazle Rabbi <fazlerabbi@zaynaxgroup.com>
2021-05-20 12:29:06 +0600

    Up7

---
+++ Assets/startQuit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startQuit : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Player");
+    }
+    public void quit()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+}
