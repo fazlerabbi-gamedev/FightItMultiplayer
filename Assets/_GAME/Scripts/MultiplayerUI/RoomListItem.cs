67c5e2fcd7c824a0d577086d067fc371c6f94e9f
Fazle Rabbi <fazlerabbi@zaynaxgroup.com>
2021-05-23 13:13:05 +0600

    Player count UI

--- Assets/_GAME/Scripts/MultiplayerUI/RoomListItem.cs
+++ Assets/_GAME/Scripts/MultiplayerUI/RoomListItem.cs
@@ -7,17 +7,23 @@ using Photon.Realtime;
 public class RoomListItem : MonoBehaviour
 {
     [SerializeField] TMP_Text text;
-
+    [SerializeField] TMP_Text playercountText;
     public RoomInfo info;
     
     public void SetUp(RoomInfo _info)
     {
         info = _info;
         text.text = _info.Name;
+        GetPlayerInfo(_info);
     }
     
     public void OnClick()
     {
         Launcher.Instance.JoinRoom(info);
+    }
+
+    public void GetPlayerInfo(RoomInfo r)
+    {
+        playercountText.text = r.PlayerCount + " / " + r.MaxPlayers;
     }
 }
