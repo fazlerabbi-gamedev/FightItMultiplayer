78ff4cad9c1d86569f9c49b5bd76415451865ab4
Fazle Rabbi <fazlerabbi@zaynaxgroup.com>
2021-06-08 09:42:43 +0600

    Fix items

--- Assets/_GAME/Scripts/Other/BreakableObject.cs
+++ Assets/_GAME/Scripts/Other/BreakableObject.cs
@@ -2,22 +2,21 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
-public class BreakableObject : MonoBehaviour, IDamagable<DamageObject> {
+public class BreakableObject : MonoBehaviour, IDamagable<DamageObject>
+{
 
 	public string hitSFX = "";
 
-	[Header ("Gameobject Destroyed")]
-	public GameObject destroyedGO;
+	[Header("Gameobject Destroyed")] public GameObject destroyedGO;
 	public bool orientToImpactDir;
 
-	[Header ("Spawn an item")]
-	public GameObject spawnItem;
+	[Header("Spawn an item")] public GameObject spawnItem;
 	public float spawnChance = 100;
 
-	[Space(10)]
-	public bool destroyOnHit;
+	[Space(10)] public bool destroyOnHit;
 
-	void Start(){
+	void Start()
+	{
 		gameObject.layer = LayerMask.NameToLayer("DestroyableObject");
 	}
 
@@ -25,60 +24,70 @@ public class BreakableObject : MonoBehaviour, IDamagable<DamageObject> {
 	public void Hit(DamageObject DO)
 	{
 		//play hit sfx
-		if (hitSFX != "") 
+		if (hitSFX != "")
 		{
-			GlobalAudioPlayer.PlaySFXAtPosition (hitSFX, transform.position);
+			GlobalAudioPlayer.PlaySFXAtPosition(hitSFX, transform.position);
 		}
 
 		//spawn destroyed gameobject version
-		if (destroyedGO != null) 
+		if (destroyedGO != null)
+
 		{
 			GameObject BrokenGO = null;
 			//	GameObject BrokenGO = GameObject.Instantiate (destroyedGO);
 			if (GameStateManager.Instance._gameState == GameState.Singleplayer)
-            {
+			{
 				BrokenGO = GameObject.Instantiate(destroyedGO);
-            }
-            else
-            {
+			}
+			else
+			{
 				BrokenGO = PhotonNetwork.Instantiate(destroyedGO.name, transform.position, Quaternion.identity);
 			}
-				
+
+			BrokenGO.transform.position = transform.position;
+
+			//GameObject BrokenGO = GameObject.Instantiate (destroyedGO);
+			//GameObject BrokenGO = PhotonNetwork.Instantiate(destroyedGO.name, transform.position, Quaternion.identity);
 			//BrokenGO.transform.position = transform.position;
 
+
 			//chance direction based on the impact direction
-			if (orientToImpactDir && DO.inflictor != null) {
+			if (orientToImpactDir && DO.inflictor != null)
+			{
 				float dir = Mathf.Sign(DO.inflictor.transform.position.x - transform.position.x);
 				BrokenGO.transform.rotation = Quaternion.LookRotation(Vector3.forward * dir);
 			}
-		}
 
-		//spawn an item
-		if (spawnItem != null) 
-		{
-			if (Random.Range (0, 100) < spawnChance) {
-				// GameObject item = GameObject.Instantiate (spawnItem);
-				// item.transform.position = transform.position;
-				GameObject item = PhotonNetwork.Instantiate(spawnItem.name, transform.position, Quaternion.identity);
 
-				//add up force to object
-				//item.GetComponent<Rigidbody>().velocity = Vector3.up * 8f;
+			//spawn an item
+			if (spawnItem != null)
+			{
+				if (Random.Range(0, 100) < spawnChance)
+				{
+					GameObject item = GameObject.Instantiate(spawnItem);
+					item.transform.position = transform.position;
+					//GameObject item = PhotonNetwork.Instantiate(spawnItem.name, transform.position, Quaternion.identity);
+
+					//	add up force to object
+					item.GetComponent<Rigidbody>().velocity = Vector3.up * 8f;
+				}
 			}
-		}
-		//destroy 
-		if (destroyOnHit) 
-		{
-			if (GameStateManager.Instance._gameState == GameState.Singleplayer)
-            {
-				Destroy(this.gameObject);
+
+			//destroy 
+			if (destroyOnHit)
+			{
+// Updated upstream
+				if (GameStateManager.Instance._gameState == GameState.Singleplayer)
+				{
+					Destroy(this.gameObject);
+				}
+				else
+				{
+					PhotonNetwork.Destroy(this.gameObject);
+				}
+
 			}
-            else
-            {
-				PhotonNetwork.Destroy(this.gameObject);
-			}
-				
 		}
 	}
-	
-	
 }
+
