26763f277ea899220be472fecc9c0796c5a0266e
Md Mehedi HAsan <hasan.zaynax@gmail.com>
2021-06-08 10:50:19 +0600

    CInematic

---
+++ Assets/_GAME/Scripts/Cinematic/CinematicPlayerAdder.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+public class CinematicPlayerAdder : MonoBehaviour
+{
+    public List<TimelineAsset> timelines;
+
+    private PlayableDirector playabledirector;
+
+    private void Start()
+    {
+        playabledirector = GetComponent<PlayableDirector>();
+        InitSequence(0);
+
+    }
+    private void InitSequence(int i)
+    {
+        playabledirector.playableAsset = timelines[i];
+        playabledirector.Play();
+    }
+}
